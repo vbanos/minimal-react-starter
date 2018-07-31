819ef9420d76fd9a5f7ba6dce304e761126a54fe
Vangelis Banos <vangelis@archive.org>
2018-07-31 17:54:20 +0000

    Import wayback-diff

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Counter from "./Counter";
+import waybackDiff from "wayback-diff";
 
 document.addEventListener("DOMContentLoaded", function() {
   ReactDOM.render(
